b4d58f9068a6b9c0a7c607480c419cae2811b59d
spebern <bernhard@specht.net>
2017-06-16 16:31:30 +0200

    initial commit

---
+++ src/run_session.py
@@ -0,0 +1,93 @@
+import pygame
+import time
+import random
+from Utils import get_screen_width_and_height
+
+screen_width, screen_height = get_screen_width_and_height()
+
+black   = (0,   0, 0)
+green   = (0, 255, 0)
+radius  = 100
+mid_pos = (screen_width // 2, screen_height // 2)
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("sounds/beep.mp3")
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+screen.fill(black)
+
+red_arrow       = pygame.image.load("images/red_arrow.png")
+red_arrow_left  = pygame.transform.rotate(red_arrow, 270)
+red_arrow_right = pygame.transform.rotate(red_arrow, 90)
+
+red_arrow_width, red_arrow_height = red_arrow_left.get_size()
+red_arrow_right_pos = (screen_width - red_arrow_width, (screen_height - red_arrow_height) // 2)
+red_arrow_left_pos  = (0                             , (screen_height - red_arrow_height) // 2)
+
+happy_smiley = pygame.image.load("images/happy_smiley.png")
+sad_smiley   = pygame.image.load("images/sad_smiley.png")
+
+smiley_width, smiley_height = happy_smiley.get_size()
+smiley_mid_pos = ((screen_width - smiley_width) // 2, (screen_height - smiley_height) // 2)
+
+
+def play_beep():
+    pygame.mixer.music.play()
+
+
+def run_trial(cue_pos_choices, with_feedback=False):
+    screen.fill(black)
+    pygame.display.update()
+    time.sleep(3)
+
+    pygame.draw.circle(screen, green, mid_pos, radius)
+    pygame.display.update()
+    time.sleep(1)
+
+    play_beep()
+
+    # ensure that each cue pos will be equally chosen
+    cue_pos = random.choice(list(cue_pos_choices.keys()))
+    cue_pos_choices[cue_pos] -= 1
+    if cue_pos_choices[cue_pos] == 0:
+        del cue_pos_choices[cue_pos]
+
+    if cue_pos == "left":
+        screen.blit(red_arrow_left, red_arrow_left_pos)
+    elif cue_pos == "right":
+        screen.blit(red_arrow_right, red_arrow_right_pos)
+    elif cue_pos == "both":
+        screen.blit(red_arrow_right, red_arrow_right_pos)
+        screen.blit(red_arrow_left, red_arrow_left_pos)
+    pygame.display.update()
+
+    time.sleep(6)
+    play_beep()
+
+    screen.fill(black)
+    pygame.display.update()
+    time.sleep(2)
+
+    if with_feedback:
+        smiley = random.choice([happy_smiley, sad_smiley])
+        screen.blit(smiley, smiley_mid_pos)
+        pygame.display.update()
+        time.sleep(3)
+
+
+def run_session(trials=75):
+    if trials % 3:
+        raise ValueError("'trials' must be devisable by 3")
+
+    cue_pos_choices = {
+        "left"  : trials // 3,
+        "right" : trials // 3,
+        "both"  : trials // 3,
+    }
+    for trial in range(0, trials):
+        run_trial(cue_pos_choices)
+
+
+if __name__ == '__main__':
+    run_session(3)
